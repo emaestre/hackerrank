19e286ddfeb38ceeced5d80ed8d34a98ee281b57
Eleazar Maestre <emaestre@rootstack.com>
2019-07-17 21:29:17 -0400

    Adding file day 1 of 30 days of code

---
+++ day1-data-types/data-types.cpp
@@ -0,0 +1,20 @@
+    
+// Declare second integer, double, and String variables.
+int a;
+double b;
+string c, line;
+// Read and save an integer, double, and String to your variables.
+cin >> a;
+cin >> b;
+getline(cin >> c, line);
+// Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+
+// Print the sum of both integer variables on a new line.
+cout << a + i << endl;
+cout << fixed << setprecision(1) << b + d << endl;
+cout << s + c + line << endl;
+// Print the sum of the double variables on a new line.
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+
