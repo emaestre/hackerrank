1e3bc62d2b152f37cddaa5fcb58137be54ad1fc1
Eleazar Maestre <emaestre@rootstack.com>
2019-07-26 16:17:13 -0400

    Adding day 23 - BST - Level Order Traversal

---
+++ 30 Days of Code/day-23-bst-level-order-traversal/level-order-traversal.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node *left, *right;
+        Node(int d)
+        {
+            data = d;
+            left = right = NULL;
+        }
+};
+class Solution
+{
+    public:
+        Node *insert(Node *root, int data)
+        {
+            if (root == NULL)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node *cur;
+                if (data <= root->data)
+                {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else
+                {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+                return root;
+            }
+        }
+
+        void levelOrder(Node *root)
+        {
+            queue<Node *> cola1;
+            Node *move;
+
+            if (root != NULL)
+            {
+                cola1.push(root);
+                while (!cola1.empty())
+                {
+                    move = cola1.front();
+                    cola1.pop();
+                    cout << move->data << " ";
+
+                    if (move->left != NULL)
+                        cola1.push(move->left);
+                    if (move->right != NULL)
+                        cola1.push(move->right);
+                }
+            }
+        }
+}; //End of Solution
+
+int main()
+{
+    Solution myTree;
+    Node *root = NULL;
+    int T, data;
+    cin >> T;
+    while (T-- > 0)
+    {
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
